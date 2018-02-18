5d0dbc48bf6ec568da095d479ae593729cb42393
Yuto Takamoto <scarletjaeger@gmail.com>
2018-02-18 14:16:41 -0500

    Remove on-load animations after 1s

--- blueleaf/static/js/login.js
+++ blueleaf/static/js/login.js
@@ -1,14 +1,5 @@
 window.onload = function () {
-    // Remove any classes that are on-load only
-    $('[class^="on-load-"], [class*=" on-load-"]').each(function () {
-        parentThis = this;
-        var classes = $(this).attr('class');
-        classes.split(" ").forEach(function (className) {
-            if (className.indexOf('on-load') === 0) {
-                //$(parentThis).removeClass(className);
-            }
-        });
-    });
+
 
     $('#login-button').on('click', function () {
         var email = $('input#email').val();
@@ -24,4 +15,19 @@ window.onload = function () {
             console.log(data);
         });
     });
+
+
+    // Run this after one second because all on-load CSS animations are <1 second
+    setTimeout(function () {
+        // Remove any classes that are on-load only
+        $('[class^="on-load-"], [class*=" on-load-"]').each(function () {
+            parentThis = this;
+            var classes = $(this).attr('class');
+            classes.split(" ").forEach(function (className) {
+                if (className.indexOf('on-load') === 0) {
+                    $(parentThis).removeClass(className);
+                }
+            });
+        });
+    }, 1000);
 };
